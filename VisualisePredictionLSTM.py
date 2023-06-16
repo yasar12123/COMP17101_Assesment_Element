6682bc2c46461f516e3626f39b5c6809ae79b52f
yasar12123 <123254904+yasar12123@users.noreply.github.com>
2023-06-16 06:02:13 +0100

    chart update

--- VisualisePredictionLSTM.py
+++ VisualisePredictionLSTM.py
@@ -1,11 +1,27 @@
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+import numpy as np
 
 #read file
-testDataWithPrediction = pd.read_csv("LSTM_predictions.csv")
+df = pd.read_csv("LSTM_predictions.csv")
 
+conditions = [ df['Prediction'] == 2,
+               df['Prediction'] == 1,
+               df['Prediction'] == 0 ]
+results = ['Bullish', 'Bearish', 'Neutral']
+df['PredictionLabel'] = np.select(conditions, results)
 
-sns.lineplot(testDataWithPrediction, x='Date', y='Close')
+positive = df[df['Prediction'] == df['NextDayTrend']]
+negatives = df[df['Prediction'] != df['NextDayTrend']]
+
+print(positive)
+print(negatives)
+
+colors = ['RED']
+sns.lineplot(df, x='Date', y='Close', color='orange')
+sns.scatterplot(positive, x='Date', y='Close' , hue='PredictionLabel', palette=colors)
+sns.scatterplot(negatives, x='Date', y='Close', hue='PredictionLabel', palette=colors)
 plt.title('BTC/USDT - Close price (test split data)')
+plt.legend()
 plt.show()
